a78ca62e6f7e0df3c8b38cdb670d2a318587ee6c
devomaa <lmao@distruzione.org>
2020-07-04 19:37:36 +0300

    Update string.random.lua

--- string.random.lua
+++ string.random.lua
@@ -1,3 +1,4 @@
+_G.stringUtils = {}
 local charset = {}
 
 -- qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890
@@ -5,9 +6,7 @@ for i = 48,  57 do table.insert(charset, string.char(i)) end
 for i = 65,  90 do table.insert(charset, string.char(i)) end
 for i = 97, 122 do table.insert(charset, string.char(i)) end
 
-function string.random(length)
-  math.randomseed(os.time())
-
+function stringUtils.random(length)
   if length > 0 then
     return string.random(length - 1) .. charset[math.random(1, #charset)]
   else
